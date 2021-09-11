690a10ac329a52109f0d6f2b799aaa0b27cb7c03
annesofiefogh <anne2z08@stud.kea.dk>
2021-09-11 11:44:59 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World!");
+        System.out.println("You suck \n" +
+                "or not\n" +
+                "ja ja da");
+
+        System.out.println("""
+                Hej
+                med
+                dig""");
+
+
+    }
+}
